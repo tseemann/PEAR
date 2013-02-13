0486e9ee354ebe53ddce55b07a8730b127960e99
Tomas Flouri <flouris@gmail.com>
2013-02-13 19:33:48 +0200

    pthreads implementation

--- src/pear-flip.c
+++ src/pear-flip.c
@@ -1206,6 +1206,7 @@ main (int argc, char * argv[])
           {
              ass = assembly_ef (fwd[i], rev[i], match_score, mismatch_score, ef, &sw);
              *(fwd[i]->qscore - 1) = ass;
+             //TODO: Probably??
            }
         }
      }
